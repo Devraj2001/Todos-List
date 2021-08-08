c79d3e6649e4bdfe53c2c2fa024621f4ade9bb14
Devraj <devraj9787@gmail.com>
2021-08-08 15:02:05 +0530

    Initail commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>
+                Hi this is Devraj Goswami here.                         
+            </p>
+            <p>
+            This is my First React Project!!!
+            </p>
+            <p>
+            Thank You for Visiting.
+            </p>
+        </div>
+    )
+}
